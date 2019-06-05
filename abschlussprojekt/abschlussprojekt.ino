d4dbd8d9772043d471731684eba9a51d87f946db
robotron10 <ulf200ls@gmx.de>
2019-06-05 15:49:38 +0200

    geändert:       Zeitbedingung + Erdfeuchtesensor + Funktion für LCD-Anzeige + Stringverkettung für LCD

--- abschlussprojekt/abschlussprojekt.ino
+++ abschlussprojekt/abschlussprojekt.ino
@@ -1,9 +1,9 @@
-// Datum: 03.06.2019
-// Autor: Stephanie Nawroth, Thorsten Graf, Fabian Roth
+// Datum: 05.06.2019
+// Autor: Stephanie Nawroth, Torsten Graf, Fabian Roth
 //
 // Thema: vollautomatisches Gewächshaus
 
-#include "DHT.h"                
+#include "DHT.h"
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
@@ -16,8 +16,8 @@
 #define TASTER_PUMPE_UNTEN 3              // Anschlußpin für den Taster zum Einschalten der Wasserpumpe unten mit ISR
 #define RELAIS_PUMPE_OBEN 4               // Anschlußpin für das Relais, welches die Wasserpumpe oben steuert
 #define RELAIS_PUMPE_UNTEN 5              // Anschlußpin für das Relais, welches die Wasserpumpe unten steuert
-#define ERDFEUCHTESENSOR_1 6              // Anschlußpin für den Erdfeuchtesensor
-#define ERDFEUCHTESENSOR_2 7              // Anschlußpin für den Erdfeuchtesensor
+#define ERDFEUCHTESENSOR_1 A0             // Anschlußpin für den Erdfeuchtesensor
+#define ERDFEUCHTESENSOR_2 A1             // Anschlußpin für den Erdfeuchtesensor
 #define TEMPERATURSENSOR_LUEFTER 8        // Anschlußpin für Temperatur- und Feuchtesensor DHT22
 #define RELAIS_LUEFTER 9
 #define RELAIS_HEIZUNG 10
@@ -29,9 +29,20 @@
 //--- Variablen
 //-------------------------------------------------------
 
-volatile int taster_status = 0;            
+volatile int taster_status = 0;
+
+unsigned long t0_meas;
+unsigned long dt_meas_ms = 2000;
+
 unsigned long t0_display;
 unsigned long dt_display_ms = 5000;
+
+unsigned long t0_erdfeuchtemessung;
+unsigned long dt_erdfeuchtemessung_ms = 5000;
+
+int limit_bodenfeuchte = 500;
+int taster;
+float temp_air, hum_air, hum_soil1, hum_soil2;
 
 //-------------------------------------------------------
 // LCD per I2C und die Adresse 0x37
@@ -39,7 +50,11 @@ unsigned long dt_display_ms = 5000;
 // Pin 5 -> SCL
 //-------------------------------------------------------
 
-LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);                               
+// Windows
+//LiquidCrystal_I2C lcd(0x3F, 20, 4 );
+
+// Linux
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 // Temperatur- und Feuchtesensor einrichten (Pin, Typ)
 DHT dht(TEMPERATURSENSOR_LUEFTER, DHTTYPE);
@@ -50,32 +65,78 @@ DHT dht(TEMPERATURSENSOR_LUEFTER, DHTTYPE);
 
 
 // ISR um die Pumpe manuell ein-/ausschalten zu können
-void taster_unterbricht() {
-   taster_status = digitalRead(TASTER_PUMPE_UNTEN);      // Tasterstatus einlesen (High/Low)
-   digitalWrite(RELAIS_PUMPE_UNTEN, taster_status);      // Relais schalten
+//void taster_unterbricht() {
+//  taster_status = digitalRead(TASTER_PUMPE_UNTEN);      // Tasterstatus einlesen (High/Low)
+//  digitalWrite(RELAIS_PUMPE_UNTEN, taster_status);      // Relais schalten
+//}
+
+//------------------------------------------------------
+//--- String ausgeben
+//------------------------------------------------------
+String make_string( String s ) {
+  while ( s.length() < 20 ) {
+    s = s + " ";
+  }
+  return s;
 }
 
+//------------------------------------------------------
+//--- Anzeige aktualisieren
+//------------------------------------------------------
+void update_lcd() {
+  lcd.setCursor(0, 0);
+
+  lcd.print(
+    make_string(
+      String( "Temperatur:  " + String(temp_air) + String( (char)223 )
+            )
+    )
+  );
+  lcd.setCursor(0, 1);
+  lcd.print("Luftfeuchte: ");
+  lcd.setCursor(13, 1);
+  lcd.print(hum_air);                             // Luftfeuchtewert ausgeben
+  lcd.setCursor(0, 2);
+  lcd.print("Erdfeuchte:  ");
+  lcd.print(hum_soil1);                             // Erdfeuchtewert ausgeben
+  lcd.setCursor(0, 3);
+  lcd.print("Pumpe: ");
+}
 
 //------------------------------------------------------
 //--- Setup des Programms, wird nur einmal ausgeführt
 //------------------------------------------------------
-
 void setup() {
-  
+
   //  LCD Setup
-  lcd.begin(20, 4);
-  lcd.setCursor(0,0);
-  lcd.print("+++ SYSTEMSTART +++");
-  
+  //lcd.init();               // Windows
+  //lcd.backlight();          // Windows
+  lcd.begin(20, 4);           // Linux
+  lcd.setCursor(0, 0);
+  lcd.print("+++ STARTING...  +++");
+
   // Temperatursensor Setup
   dht.begin();
-  
+  hum_air = dht.readHumidity();                // Lesen der Luftfeuchtigkeit und speichern in die Variable h
+  hum_soil1 = analogRead( ERDFEUCHTESENSOR_1 ); // Lesen der Erdfeuchtigkeit
+  hum_soil2 = analogRead( ERDFEUCHTESENSOR_2 ); // Lesen der Erdfeuchtigkeit
+  temp_air = dht.readTemperature();            // Lesen der Temperatur in °C und speichern in die Variable t
+
   // Pumpen Setup
   pinMode(RELAIS_PUMPE_UNTEN, OUTPUT);                  // Relais für die Wasserpume
   pinMode(TASTER_PUMPE_UNTEN, INPUT);
-  
-  // ISR einrichten    
-  attachInterrupt(0, taster_unterbricht, LOW);
+
+  // ISR einrichten
+  //  attachInterrupt(0, taster_unterbricht, LOW);
+
+  t0_meas = millis();
+  t0_display = millis();
+  t0_erdfeuchtemessung = millis();
+
+  taster = digitalRead(TASTER_PUMPE_UNTEN);         // Tasterzustand an Pin 3 lesen
+
+  lcd.setCursor(0, 1); // Spalte, Zeile
+  lcd.print("+++ ...COMPLETE! +++");
 }
 
 //-------------------------------------------------------
@@ -84,52 +145,44 @@ void setup() {
 
 void loop() {
 
-  int taster = digitalRead(TASTER_PUMPE_UNTEN);         // Tasterzustand an Pin 3 lesen
+  /*  if ( taster == LOW ) and ( digitalRead(TASTER_PUMPE_UNTEN) == HIGH ) {
+    taster == HIGH;
+    }
+  */
 
-  
-  /********************************( Tempsensor )*********************************************/ 
-  delay(2000);                                                
-  float h = dht.readHumidity();             // Lesen der Luftfeuchtigkeit und speichern in die Variable h
-  float t = dht.readTemperature();          // Lesen der Temperatur in °C und speichern in die Variable t
-  
+  int messwert_bodenfeuchte_1 = analogRead( ERDFEUCHTESENSOR_1 ); // Werte zwischen 0..1023
+  int messwert_bodenfeuchte_2 = analogRead( ERDFEUCHTESENSOR_2 ); // Werte zwischen 0..1023
+
+  /********************************( Tempsensor )*********************************************/
+  if ( ( millis() - t0_meas ) > dt_meas_ms ) {
+    t0_meas = millis();
+    hum_soil1 = analogRead( ERDFEUCHTESENSOR_1 );   // Lesen der Erdfeuchtigkeit 0..1023
+    hum_soil2 = analogRead( ERDFEUCHTESENSOR_2 );   // Lesen der Erdfeuchtigkeit
+    hum_air = dht.readHumidity();                   // Lesen der Luftfeuchtigkeit und speichern in die Variable h
+    temp_air = dht.readTemperature();               // Lesen der Temperatur in °C und speichern in die Variable t
+  }
+
   //if (isnan(h) || isnan(t)) {             // is not a number
   //  lcd.setCursor(0, 1);
-  //  lcd.print("Fehler beim auslesen des Sensors!");
+  //  lcd.print("Fehler Luftsensor!");
   //  return;
   //}
-  
-/********************************( Display Ausgabe )*********************************************/
-  if ( millis() - t0_display > dt_display_ms ) {
-     t0_display = millis();
-     lcd.setCursor(0,0);         
-     lcd.print("Temperatur:  ");
-     lcd.setCursor(13,0);
-     lcd.print(t);                             // Temperaturwert ausgeben
-     lcd.setCursor(18,0);
-     lcd.print((char)223);                     // '°' Zeichen
-     lcd.setCursor(0,1);         
-     lcd.print("Luftfeuchte: ");
-     lcd.setCursor(13,1);
-     lcd.print(h);                             // Luftfeuchtewert ausgeben
-     lcd.setCursor(0,2);
-     lcd.print("Erdfeuchte: ");
-     //lcd.print(String(millis(), DEC));
-     lcd.setCursor(0,3);
-     lcd.print("Pumpe: ");
+
+  /********************************( Display Ausgabe )*********************************************/
+  if ( ( millis() - t0_display ) > dt_display_ms ) {
+    t0_display = millis();
+    update_lcd();
   }
 
-/********************************( Wasserpumpe )*********************************************/
-     // Bedingung für die Wasserpumpe(Relais)
-     if (taster == HIGH){                               // bei Tastendruck - Relais ein
-          digitalWrite(RELAIS_PUMPE_UNTEN, HIGH);
-          lcd.setCursor(13,3);
-          lcd.print("EIN");                             // Ausgabe auf den Display
-     } else {
-          digitalWrite(RELAIS_PUMPE_UNTEN, LOW);        // kein Tastendruck - Relais aus
-          lcd.setCursor(13,3);
-          lcd.print("AUS");                             // Ausgabe auf den Display
-     }
-    
+  /********************************( Wasserpumpe -- Erdfeuchtesensoren)*********************************************/
+  if ( ( millis() - t0_erdfeuchtemessung ) > dt_erdfeuchtemessung_ms ) {
+    t0_erdfeuchtemessung = millis();
+
+    if (messwert_bodenfeuchte_1 > limit_bodenfeuchte ) {
+      digitalWrite(RELAIS_PUMPE_UNTEN, HIGH);
+      lcd.setCursor(13, 3);
+      lcd.print("EIN");                             // Ausgabe auf den Display
+    }
   }
 
-
+}
