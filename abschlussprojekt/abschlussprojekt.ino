1d358415fc3a238db924fc2381c12b28013776a0
robotron10 <ulf200ls@gmx.de>
2019-06-05 10:00:59 +0200

    geändert:       defines angelegt/ Zeitbedingung für Displayanzeige

--- abschlussprojekt/abschlussprojekt.ino
+++ abschlussprojekt/abschlussprojekt.ino
@@ -7,6 +7,10 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+//-------------------------------------------------------
+//--- Makros -> Anschlußpins der Peripherie
+//-------------------------------------------------------
+
 #define DHTTYPE DHT22                     // Typ des Temperatur- und Feuchtesensors  
 #define TASTER_PUMPE_OBEN 2               // Anschlußpin für den Taster zum Einschalten der Wasserpumpe oben mit ISR                     
 #define TASTER_PUMPE_UNTEN 3              // Anschlußpin für den Taster zum Einschalten der Wasserpumpe unten mit ISR
@@ -21,17 +25,41 @@
 #define HELLIGKEITSSENSOR_UV_LED 12
 #define RELAIS_UV_LED 13
 
-volatile int taster_status = 0;            // 
+//-------------------------------------------------------
+//--- Variablen
+//-------------------------------------------------------
 
+volatile int taster_status = 0;            
+unsigned long t0_display;
+unsigned long dt_display_ms = 5000;
+
+//-------------------------------------------------------
 // LCD per I2C und die Adresse 0x37
 // Pin 4 -> SDA
 // Pin 5 -> SCL
+//-------------------------------------------------------
+
 LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);                               
 
 // Temperatur- und Feuchtesensor einrichten (Pin, Typ)
 DHT dht(TEMPERATURSENSOR_LUEFTER, DHTTYPE);
 
-// Setup des Programms, wird nur einmal ausgeführt
+//-------------------------------------------------------
+//--- Funktionen
+//-------------------------------------------------------
+
+
+// ISR um die Pumpe manuell ein-/ausschalten zu können
+void taster_unterbricht() {
+   taster_status = digitalRead(TASTER_PUMPE_UNTEN);      // Tasterstatus einlesen (High/Low)
+   digitalWrite(RELAIS_PUMPE_UNTEN, taster_status);      // Relais schalten
+}
+
+
+//------------------------------------------------------
+//--- Setup des Programms, wird nur einmal ausgeführt
+//------------------------------------------------------
+
 void setup() {
   
   //  LCD Setup
@@ -50,7 +78,10 @@ void setup() {
   attachInterrupt(0, taster_unterbricht, LOW);
 }
 
-// Schleife des Programms
+//-------------------------------------------------------
+//--- Schleife des Programms
+//-------------------------------------------------------
+
 void loop() {
 
   int taster = digitalRead(TASTER_PUMPE_UNTEN);         // Tasterzustand an Pin 3 lesen
@@ -68,38 +99,37 @@ void loop() {
   //}
   
 /********************************( Display Ausgabe )*********************************************/
-  lcd.setCursor(0,0);         
-  lcd.print("Temperatur:  ");
-  lcd.setCursor(13,0);
-  lcd.print(t);                             // Temperaturwert ausgeben
-  lcd.setCursor(18,0);
-  lcd.print((char)223);                     // '°' Zeichen
-  lcd.setCursor(0,1);         
-  lcd.print("Luftfeuchte: ");
-  lcd.setCursor(13,1);
-  lcd.print(h);                             // Luftfeuchtewert ausgeben
-  lcd.setCursor(0,2);
-  lcd.print("Erdfeuchte: ");
-  //lcd.print(String(millis(), DEC));
-  lcd.setCursor(0,3);
-  lcd.print("Pumpe: ");
+  if ( millis() - t0_display > dt_display_ms ) {
+     t0_display = millis();
+     lcd.setCursor(0,0);         
+     lcd.print("Temperatur:  ");
+     lcd.setCursor(13,0);
+     lcd.print(t);                             // Temperaturwert ausgeben
+     lcd.setCursor(18,0);
+     lcd.print((char)223);                     // '°' Zeichen
+     lcd.setCursor(0,1);         
+     lcd.print("Luftfeuchte: ");
+     lcd.setCursor(13,1);
+     lcd.print(h);                             // Luftfeuchtewert ausgeben
+     lcd.setCursor(0,2);
+     lcd.print("Erdfeuchte: ");
+     //lcd.print(String(millis(), DEC));
+     lcd.setCursor(0,3);
+     lcd.print("Pumpe: ");
+  }
 
 /********************************( Wasserpumpe )*********************************************/
      // Bedingung für die Wasserpumpe(Relais)
-     if (taster == HIGH){                   // bei Tastendruck - Relais ein
+     if (taster == HIGH){                               // bei Tastendruck - Relais ein
           digitalWrite(RELAIS_PUMPE_UNTEN, HIGH);
           lcd.setCursor(13,3);
-          lcd.print("EIN");                 // Ausgabe auf den Display
+          lcd.print("EIN");                             // Ausgabe auf den Display
      } else {
           digitalWrite(RELAIS_PUMPE_UNTEN, LOW);        // kein Tastendruck - Relais aus
           lcd.setCursor(13,3);
-          lcd.print("AUS");                 // Ausgabe auf den Display
+          lcd.print("AUS");                             // Ausgabe auf den Display
      }
     
   }
 
-  // ISR um die Pumpe manuell ein-/ausschalten zu können
-void taster_unterbricht() {
-   taster_status = digitalRead(TASTER_PUMPE_UNTEN);      // Tasterstatus einlesen (High/Low)
-   digitalWrite(RELAIS_PUMPE_UNTEN, taster_status);      // Relais schalten
-}
+
